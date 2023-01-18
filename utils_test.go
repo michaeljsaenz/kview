d4f85cbaac292e5b758393966c58079474bce643
Michael <mr.michaeljsaenz@gmail.com>
2023-01-17 22:52:15 -0600

    add test to utils

--- utils_test.go
+++ utils_test.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"testing"
+)
+
+func TestConvertMapToString(t *testing.T) {
+	testMap := make(map[string]string)
+	testMap["key"] = "value"
+	expectedString := "key=\"value\"\n"
+	returnString := convertMapToString(testMap)
+	if expectedString != returnString {
+		t.Errorf("Did not get expected result. Got '%s', wanted '%s'", returnString, expectedString)
+	}
+}
